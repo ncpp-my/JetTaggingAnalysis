9450aab7373ee0a9b4bc50d2cb65f38cb9b0f1eb
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2019-10-08 04:36:14 +0200

    add config files

---
+++ NanoAODAnalyzer/scripts/ProcNanoAODSkim_Common.py
@@ -0,0 +1,2 @@
+inputListDir     = "/afs/cern.ch/work/n/nbinnorj/AnaJetTagging/CMSSW_10_2_15/src/JetTaggingAnalysis/SampleListsEOS/"
+outputListDir    = "root://eosuser.cern.ch//eos/user/n/nbinnorj/JetTaggingAna/Ntuples/"
---
+++ NanoAODAnalyzer/scripts/ProcNanoAODSkim_MC2017_cfg.py
@@ -0,0 +1,35 @@
+import sys
+import optparse
+import FWCore.ParameterSet.Config as cms
+import FWCore.PythonUtilities.LumiList as LumiList
+import FWCore.ParameterSet.Types as CfgTypes
+sys.path.insert(0, 'JetTaggingAnalysis/NanoAODAnalyzer/scripts/')
+from ProcNanoAODSkim_Common import *
+#
+# Basic Configuration
+#
+era              = "2017"
+debug            = False
+nevents          = -1
+#
+# ParameterSets for use in bin/<script>.cc 
+#
+process = cms.PSet()
+#
+# LumiMask
+#
+process.inputs = cms.PSet(
+  lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange()),
+)
+#
+#
+#
+process.ProcessNanoAOD_JetTagging = cms.PSet(
+  debug            = cms.bool(debug),
+  maxEvents        = cms.int32(int(nevents)),
+  inputListDir     = cms.string(inputListDir),
+  outputListDir    = cms.string(outputListDir),
+  era              = cms.string(era),
+  blind            = cms.bool(False),
+  isMC             = cms.bool(True),
+)
